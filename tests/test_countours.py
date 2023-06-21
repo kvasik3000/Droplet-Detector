c2d04e9d8813bec15a87d47fd3202e774e76b989
Andrey <114009968+kvasik3000@users.noreply.github.com>
2023-06-22 05:52:26 +0700

    Update test_countours.py

--- tests/test_countours.py
+++ tests/test_countours.py
@@ -13,18 +13,22 @@ def test_get_contours():
 
     # Проверяем результаты
     assert len(contours) == 1  # Ожидается один контур
-    assert cv2.contourArea(contours[0]) == 1743.0  # Ожидается площадь контура равная 1743.0
+    assert cv2.contourArea(contours[0]) == 1743.0
+    # Ожидается площадь контура равная 1743.0
 
     # Проверяем длину контура
     contour_length = cv2.arcLength(contours[0], True)
-    assert contour_length == 165.65685415267944  # Ожидается длина контура равная 176.67570447921753
+    assert contour_length == 165.65685415267944
+    # Ожидается длина контура равная 176.67570447921753
 
     # Проверяем тип возвращаемого значения
-    assert isinstance(contours, (list, tuple))  # Ожидается, что возвращаемое значение будет списком или кортежем
+    assert isinstance(contours, (list, tuple))
+    # Ожидается, что возвращаемое значение будет списком или кортежем
 
     # Проверяем, что контуры имеют правильный тип данных
     assert all(
-        isinstance(contour, np.ndarray) for contour in contours)  # Ожидается, что каждый контур будет массивом NumPy
+        isinstance(contour, np.ndarray) for contour in contours)
+    # Ожидается, что каждый контур будет массивом NumPy
 
 
 # Запускаем тест
