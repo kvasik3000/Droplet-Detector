f363be34f51df14f2e9baf60abd64229d77b0509
Andrey <114009968+kvasik3000@users.noreply.github.com>
2023-06-24 05:09:20 +0700

    Update calculate.py

--- calculate.py
+++ calculate.py
@@ -1,21 +1,40 @@
 import cv2  # type: ignore
 from do_xslx import do_df
+from typing import List
 
-sum_area_s = 0
+
+sum_area_s: float = 0
 
 
-def get_sum():
+def get_sum() -> float:
+    """
+    Возвращает текущую сумму площадей.
+    """
     return sum_area_s
 
 
-def update_sum(new_value):
+def update_sum(new_value: float) -> None:
+    """
+    Обновляет значение суммы площадей.
+
+    Args:
+        new_value: Новое значение суммы площадей (тип: float).
+    """
     global sum_area_s
     sum_area_s = new_value
 
 
-def calculate_area(contours):
-    # global sum_area_s
-    all_area = 0
+def calculate_area(contours: List[List[float]]) -> float:
+    """
+    Вычисляет общую площадь для заданных контуров.
+
+    Args:
+        contours: Список контуров (тип: List[List[float]]).
+
+    Returns:
+        Общая площадь (тип: float).
+    """
+    all_area: float = 0
 
     for contour in contours:
         all_area += cv2.contourArea(contour)
