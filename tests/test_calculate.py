9b6e97fcc27a6c2ad5febf40498e7ee54776b2e6
Andrey <114009968+kvasik3000@users.noreply.github.com>
2023-06-23 20:34:52 +0700

    Update test_calculate.py

--- tests/test_calculate.py
+++ tests/test_calculate.py
@@ -1,7 +1,7 @@
 import numpy as np
 import pytest
 import sys
-sys.path.insert(0,'..')  # noqa
+sys.path.append(os.path.abspath("Droplet-Detector/"))  # noqa
 
 from calculate import calculate_area, get_sum, update_sum  # type: ignore  # noqa
 
